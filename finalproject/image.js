f05efa260b69699b8475eee8ba9cb6706df61115
Peihong Guo <peihongguo@gmail.com>
2013-11-29 20:28:02 -0600

    final project in progress

--- finalproject/image.js
+++ finalproject/image.js
@@ -88,6 +88,7 @@ Color.interpolate = function(c1, c2, t)
     return c1.mul(t).add(c2.mul(1-t));
 };
 
+/*
 function RGBAImage( w, h, data )
 {
     this.channels = 4;
@@ -155,14 +156,142 @@ RGBAImage.prototype.toImageData = function( ctx ) {
     imgData.data.set(this.data);
     return imgData;
 };
+*/
+
+function ImageBase(w, h) {
+    this.channels = 4;
+    this.w = w;
+    this.h = h;
+    this.data = undefined;
+}
+
+ImageBase.prototyp.map = function( fun ) {
+    for(var y=0;y<this.h;y++) {
+        for(var x=0;x<this.w;x++) {
+            fun(x, y, this.getPixel(x, y));
+        }
+    }
+};
+
+ImageBase.prototype.getPixel = function(x, y) {
+    var idx = (y * this.w + x) * this.channels;
+    return new Color(
+        this.data[idx+0],
+        this.data[idx+1],
+        this.data[idx+2],
+        this.data[idx+3]
+    );
+};
+
+// bilinear sample of the image
+ImageBase.prototype.sample = function(x, y) {
+    var w = this.w, h = this.h;
+    var ty = Math.floor(y);
+    var dy = Math.ceil(y);
+
+    var lx = Math.floor(x);
+    var rx = Math.ceil(x);
+
+    var fx = x - lx;
+    var fy = y - ty;
+
+    var c = this.getPixel(lx, ty).mul((1-fy) * (1-fx))
+        .add(this.getPixel(lx, dy).mul(fy * (1-fx)))
+        .add(this.getPixel(rx, ty).mul((1-fy) * fx))
+        .add(this.getPixel(rx, dy).mul(fy * fx));
+
+    c.clamp();
+
+    return c;
+};
+
+ImageBase.prototype.setPixel = function(x, y, c) {
+    var idx = (y * this.w + x) * this.channels;
+    this.data[idx] = c.r;
+    this.data[idx+1] = c.g;
+    this.data[idx+2] = c.b;
+    this.data[idx+3] = c.a;
+};
+
+function RGBAImagef(w, h, data) {
+    var that = new ImageBase(w, h);
+    that.data = new Float32Array(w*h*that.channels);
+    data && that.data.set(data);
+
+    that.toRGBAImage = function() {
+        var w = this.w, h = this.h;
+        var img = new RGBAImage(w, h);
+        for(var y=0;y<h;y++) {
+            for(var x=0;x<w;x++) {
+                var c = this.getPixel(x, y);
+                c.round();
+                c.clamp();
+                img.setPixel(x, y, c);
+            }
+        }
+        return img;
+    };
+
+    return that;
+}
+
+ImageBase.prototype.setImage = function( img ) {
+    this.w = img.w;
+    this.h = img.h;
+    this.data = new Uint8Array(this.w*this.h*this.channels);
+    this.data.set(img.data);
+    return this;
+}
+
+function RGBAImage( w, h, data )
+{
+    var that = new ImageBase(w, h);
+    that.data = new Uint8Array(w*h*that.channels);
+    data && that.data.set(data);
+
+    that.toRGBAImagef = function() {
+        var w = this.w, h = this.h;
+        var img = new RGBAImagef(w, h);
+        for(var y=0;y<h;y++) {
+            for(var x=0;x<w;x++) {
+                img.setPixel(x, y, this.getPixel(x, y));
+            }
+        }
+        return img;
+    };
+
+    that.uploadTexture = function( ctx, texId )
+    {
+        var w = this.w;
+        var h = this.h;
+
+        ctx.bindTexture(ctx.TEXTURE_2D, texId);
+        ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_MIN_FILTER, ctx.NEAREST);
+        ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_MAG_FILTER, ctx.NEAREST);
+        ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_WRAP_S, ctx.CLAMP_TO_EDGE);
+        ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_WRAP_T, ctx.CLAMP_TO_EDGE);
+        ctx.texImage2D(ctx.TEXTURE_2D, 0,  ctx.RGBA, w, h, 0, ctx.RGBA, ctx.UNSIGNED_BYTE, this.data);
+    };
+
+    that.toImageData = function( ctx ) {
+        var imgData = ctx.createImageData(this.w, this.h);
+        imgData.data.set(this.data);
+        return imgData;
+    };
+
+    return that;
+}
 
 /* get RGBA image data from the passed image object */
-RGBAImage.fromImage = function( img, ctx ) {
+RGBAImage.fromImage = function( img, cvs ) {
     var w = img.width;
     var h = img.height;
 
     // resize the canvas for drawing
-    canvasresize(w, h);
+    cvs.width = w;
+    cvs.height = h;
+
+    var ctx = cvs.getContext('2d');
 
     // render the image to the canvas in order to obtain image data
     ctx.drawImage(img, 0, 0);
--- finalproject/main.js
+++ finalproject/main.js
@@ -19,7 +19,7 @@ function uploadImages( files ) {
             reader.onload = function (event) {
                 var img = new Image();
                 img.onload = function(e) {
-                    var I = RGBAImage.fromImage(this, context);
+                    var I = RGBAImage.fromImage(this, canvas);
                     I.filename = filename;
                     I.deltaT = parseDeltaT(filename);
                     sourceImages.push(I);
@@ -78,6 +78,83 @@ function sampleSourceImages( sourceImages ) {
     };
 }
 
+function gsolve(Z, exposures, l, w) {
+    var n = 256;
+    var nsamples = Z[0].length;
+    var nexposures = exposures.length;
+
+    // row major storage
+    var A = numeric.rep([nsamples*nexposures+n+1, n+nsamples], 0);
+    var b = numeric.rep([nsamples*nexposures+n+1], 0);
+
+    // fill in the entries
+    var k = 0;
+    for(var i=0;i<nsamples;i++) {
+        for(var j=0;j<nexposures;j++) {
+            var lev = Z[j][i];
+            var wij = w[lev];
+            A[k][lev] = wij;
+            A[k][n+i] = -wij;
+            b[k] = wij * exposures[j];
+            k++;
+        }
+    }
+
+    // fix the curve by setting its middle value to 0
+    A[k++][129] = 1;
+
+    // smoothness
+    for(var i=0;i<n-2;i++) {
+        var wi = w[i+1];
+        A[k][i] = l * wi;
+        A[k][i+1] = -2*l*wi;
+        A[k][i+2] = l*wi;
+        k++;
+    }
+
+    var At = numeric.ccsSparse(numeric.transpose(A));
+    var A = numeric.ccsSparse(A);
+    var AtA = numeric.ccsDot(At, A);
+    var Atb = numeric.ccsMV(At, b);
+
+    var g = numeric.ccsLUPSolve(numeric.ccsLUP(AtA), Atb);
+    return g;
+}
+
+function hdr(sourceimgs, gr, gg, gb, w) {
+    var tmpimg = sourceimgs[0];
+    var w = tmpimg.w, h = tmpimg.h;
+    var npixels = tmpimg.w * tmpimg.h;
+    var nexposures = sourceImages.length;
+
+    var hdrmap = new RGBAImagef(w, h);
+    var summap = new RGBAImagef(w, h);
+    var m = new RGBAImagef(w, h);
+
+    for(var i=0;i<nexposures;i++) {
+        var si = sourceimgs[i];
+        // accumulate the weights in the sum map
+        si.map(function(x, y, c) {
+            var wr = w[c.r];
+            var wg = w[c.g];
+            var wb = w[c.b];
+            var sc = summap.getPixel(x, y);
+            summap.setPixel(x, y, new Color(sc.r + wr, sc.g + wg, sc.b + wb, 0));
+        });
+
+        si.map(function(x, y, c) {
+            var dr = gr[c.r] - si.deltaT;
+            var dg = gg[c.g] - si.deltaT;
+            var db = gb[c.b] - si.deltaT;
+            m.setPixel(x, y, new Color(dr, dg, db, 0));
+        });
+
+        // handle saturated pixels
+    }
+
+    //
+}
+
 function generateRadianceMap() {
     var tmpimg = sourceImages[0];
     var w = tmpimg.w, h = tmpimg.h;
@@ -93,15 +170,29 @@ function generateRadianceMap() {
         weights[i] = (function(z, zmin, zmax){
             if (z <= 0.5 * (zmin + zmax))
                 // never let the weights be zero because that would influence the equation system!!!
-                w = ((z - zmin) + 1);
+                return ((z - zmin) + 1);
             else
-                w = ((zmax - z) + 1);
-
-        })(i, 1, 256);
+                return ((zmax - z) + 1);
+        })(i, 0, 255);
     }
 
     // sample the source images
     var samples = sampleSourceImages( sourceImages );
+
+    var exposures = [];
+    for(var i=0;i<nexposures;i++) {
+        exposures.push(Math.log(sourceImages[i].deltaT));
+    }
+
+    console.log(exposures);
+
+    // solve for the curve
+    var gr = gsolve(samples.zr, exposures, lmda, weights);
+    var gg = gsolve(samples.zg, exposures, lmda, weights);
+    var gb = gsolve(samples.zb, exposures, lmda, weights);
+
+    // assemble the hdr radiance map
+    var hdrmap = hdr(sourceImages, gr, gg, gb, w);
 }
 
 window.onload = (function(){
